91f8e762c5593a307ae38a0ad7d50f72a25d0bd8
nnabicse <nnabi.cse@gmail.com>
2022-03-10 21:24:06 +0600

    liked posts header issue solved

--- js/app.js
+++ js/app.js
@@ -32,10 +32,12 @@ const displayContent = (text) => {
 
 const switchTab = (id) => {
     if (id === "posts") {
+        document.getElementById( "liked-post-header" ).style.display = "none";
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
     } else if (id === "liked") {
+        document.getElementById( "liked-post-header" ).style.display = "block";
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "reported" ).style.display = "none";
